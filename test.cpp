b4bf5e80c5f42e738f39aa9bf3949bc1f5452205
snow011307 <egawayuuki0113@gmail.com>
2025-05-17 23:40:47 +0900

    HelloWorldと計算とScanfを追加

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World \n");
+
+	int a;
+	int b;
+
+	printf("① 数字を入力してください：");
+	scanf("%d", &a);
+	printf("② 数字を入力してください：");
+	scanf("%d", &b);
+	printf("足し算：%d（a）+ %d（b）= %d \n", a, b, a+b);
+	printf("引き算：%d（a）- %d（b）= %d \n", a, b, a - b);
+	printf("掛け算：%d（a）* %d（b）= %d \n", a, b, a * b);
+	if (a == 0 && b == 0) {
+		printf("割り算：%d（a）/ %d（b）= （0/0は計算できません）", a, b);
+	}
+	else {
+		printf("割り算：%d（a）/ %d（b）= %d", a, b, a / b);
+	}
+}
